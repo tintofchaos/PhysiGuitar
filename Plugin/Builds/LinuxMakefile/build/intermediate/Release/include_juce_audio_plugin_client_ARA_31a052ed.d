6748c806b5a948c512e7ce14aa53a67fa5295d7f
tintofchaos <77617392+tintofchaos@users.noreply.github.com>
2023-03-18 15:13:04 +0200

    Delete Plugin/Builds/LinuxMakefile/build directory

--- Plugin/Builds/LinuxMakefile/build/intermediate/Release/include_juce_audio_plugin_client_ARA_31a052ed.d
+++
@@ -1,6 +0,0 @@
-build/intermediate/Release/include_juce_audio_plugin_client_ARA_31a052ed.o: \
- ../../JuceLibraryCode/include_juce_audio_plugin_client_ARA.cpp \
- /home/notroot/juce-7.0.2-linux/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_ARA.cpp \
- /home/notroot/juce-7.0.2-linux/JUCE/modules/juce_audio_plugin_client/ARA/juce_ARA_Wrapper.cpp \
- /home/notroot/juce-7.0.2-linux/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
- /home/notroot/juce-7.0.2-linux/JUCE/modules/juce_audio_plugin_client/ARA/../utility/juce_CheckSettingMacros.h
