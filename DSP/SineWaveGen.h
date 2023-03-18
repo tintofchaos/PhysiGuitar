a5b7b44eda60982bb45212b43a2c244938b12a37
tintofchaos <77617392+tintofchaos@users.noreply.github.com>
2023-03-18 15:08:00 +0200

    Add files via upload
    Allows more than one instance at a time without crashing

---
+++ DSP/SineWaveGen.h
@@ -0,0 +1,47 @@
+#include <math.h>
+
+#define WAVETABLE_SIZE 2048
+
+typedef struct {
+    float wavetable[WAVETABLE_SIZE];
+    unsigned long sample_rate;
+    float freq;
+    float wavetable_pos;
+} SineWaveGen ;
+
+void sine_init(SineWaveGen *sine, unsigned long sample_rate) {
+    sine->sample_rate = sample_rate;
+    sine->wavetable_pos = 0;
+
+    float freq = (float) sample_rate / (float) WAVETABLE_SIZE;
+
+    for (int i = 0; i < WAVETABLE_SIZE; i++)
+        sine->wavetable[i] = sinf(2.f * M_PI * freq * ( (float) i / (float) sample_rate) );
+
+}
+
+void sine_setfrequency(SineWaveGen *sine, float freq) {
+    sine->freq = freq;
+}
+
+void sine_reset(SineWaveGen *sine) {
+    sine->wavetable_pos = 0;
+}
+
+float sine_process(SineWaveGen *sine) {
+    float freq = (float) sine->sample_rate / (float) WAVETABLE_SIZE;
+
+    float low = floorf(sine->wavetable_pos);
+    float high = ceilf(sine->wavetable_pos);
+    float output = sine->wavetable[(int)sine->wavetable_pos];
+    
+    if (low != high)
+        output = (sine->wavetable_pos - low) * sine->wavetable[(int)high] + (high - sine->wavetable_pos) * sine->wavetable[(int)low];
+        
+    sine->wavetable_pos += sine->freq / freq;
+
+    if (sine->wavetable_pos > (float) WAVETABLE_SIZE - 1.f)
+        sine->wavetable_pos -= (float) WAVETABLE_SIZE - 1.f;
+
+    return output;
+}
---
+++ Plugin/Source/GuitarVoice.h
@@ -0,0 +1,128 @@
+/*
+  ==============================================================================
+
+    GuitarVoice.h
+    Created: 16 Mar 2023 7:24:51pm
+    Author:  notroot
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "GuitarSound.h"
+
+#include "DSP/PluckedString.h"
+#include "DSP/Pickup.h"
+
+PluckedString *strings[6];
+Pickup *pickups[6];
+
+int n = 0;
+
+class GuitarVoice : public juce::SynthesiserVoice
+{
+    int success;
+    int initialized; // check if a note has been played
+public:
+    GuitarVoice()
+    {
+        initialized = 0;
+        if (n > 5) n = 0;
+        string_init(&guitar_string, (int) getSampleRate(), 440.f );
+        string_setstiffness(&guitar_string, 0.01);
+        string_setposition(&guitar_string, 0.23);
+        string_setdamping(&guitar_string, 0.35);
+        string_setmuting(&guitar_string, 0.2);
+        string_setfrequency(&guitar_string, 20.f);
+        
+        if (pickup_init(&pickup, (int) getSampleRate() ) ) success = 1;
+        pickup_setposition(&pickup, 6.375 / 25.5);
+        pickup_setpickup(&pickup, 5000.f, 0.707, PICKUP_PRESET_STRAT, 1.f);
+        pickups[n] = &pickup;
+        strings[n++] = &guitar_string;
+        gain = 0;
+        release = 0;
+        pitch_bend = 0;
+    }
+    
+    ~GuitarVoice()
+    {
+        pickup_free(&pickup);
+        n = 0;
+    }
+    
+    bool canPlaySound(juce::SynthesiserSound* sound) override
+    {
+        return dynamic_cast<GuitarSound*> (sound) != NULL && success == 1;
+    }
+    
+    void startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound*, int) override
+    {
+        midi_note = (float) midiNoteNumber;
+        auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz (midiNoteNumber);
+        string_noteon(&guitar_string, cyclesPerSecond, velocity);
+        pickup_setfrequency(&pickup, cyclesPerSecond);
+        release = 0;
+        gain = 0;
+        initialized = 1;
+    }
+
+    void stopNote (float, bool allowTailOff) override
+    {
+        release = 1;
+    }    
+    
+    void renderNextBlock (juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
+    {
+        while (--numSamples >= 0)
+        {
+            auto currentSample = pickup_process(&pickup, string_process(&guitar_string) ) * gain;
+                        
+            if (!initialized) currentSample = 0;
+                        
+            for (auto i = outputBuffer.getNumChannels(); --i >= 0;)                     
+                outputBuffer.addSample (i, startSample, currentSample);
+                    
+            ++startSample;
+                    
+            if (0 < gain && release) gain -= 1.f / (getSampleRate() / 50.f);
+                    
+            if (1 > gain && !release) gain += 1.f / (getSampleRate() / 50.f);
+
+                    
+            if (gain <= 0.005 && release)
+            {
+                string_noteoff(&guitar_string);
+                clearCurrentNote();   
+                break;
+            }
+            
+        }
+    }
+    
+    void pitchWheelMoved (int newPitchWheelValue) override
+    {
+        if (newPitchWheelValue - 8192 > 0)
+            pitch_bend = (float) (newPitchWheelValue - 8192) / 8191.f * 2.f;
+                    
+        float frequency = 440.f * powf(2.f, (midi_note + pitch_bend - 69.f) / 12.f);
+        
+        pickup_setfrequency(&pickup, frequency);
+        string_setfrequency(&guitar_string, frequency);
+
+    }
+    
+    void controllerMoved (int, int) override
+    {
+    }
+    
+    using juce::SynthesiserVoice::renderNextBlock;
+    
+    PluckedString guitar_string;
+    Pickup pickup;
+    float gain;
+    int release;
+    float midi_note;
+    float pitch_bend;
+};
